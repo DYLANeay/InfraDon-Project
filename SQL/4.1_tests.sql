520704ad80095980f8db640dc5b593f1b306aee2
DYLANeay <dylan.eray6@gmail.com>
2025-05-01 10:39:45 +0200

    test update

--- SQL/4.1_tests.sql
+++ SQL/4.1_tests.sql
@@ -1,12 +1,13 @@
-SELECT * FROM assurance
-SELECT * FROM hopital 
-SELECT * FROM medecin 
+-- Active: 1741351947031@@127.0.0.1@5432@hopital
+-- Active: 1741351947031@@127.0.0.1@5432@hopital1@@127.0.0.1@5432@hopital
+SELECT * FROM assurance;
+SELECT * FROM hopital;
+SELECT * FROM medecin;
 
 
-SELECT * FROM medecin_hopital_travaille 
-SELECT * FROM medicament 
-SELECT * FROM patient 
-SELECT * FROM personne 
-
-SELECT * FROM prescription p 
-SELECT * FROM rendez_vous 
+SELECT * FROM medecin_hopital_travaille ;
+SELECT * FROM medicament ;
+SELECT * FROM patient ;
+SELECT * FROM personne ;
+SELECT * FROM prescription p; 
+SELECT * FROM rendez_vous; 
