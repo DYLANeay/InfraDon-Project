97d4fd954c0c7b7bb7dcb4c2215cbc13981df3b3
DYLANeay <dylan.eray6@gmail.com>
2025-05-01 11:01:38 +0200

    2nd update of roles

--- SQL/5.1_roles-utilisateur.sql
+++ SQL/5.1_roles-utilisateur.sql
@@ -3,11 +3,10 @@
 CREATE ROLE medecin;
 GRANT INSERT, SELECT, UPDATE, DELETE ON prescription TO medecin;
 GRANT SELECT ON ALL TABLES TO medecin;
-
-
+--------------------------------------------
 CREATE ROLE administrateur;
 GRANT INSERT, SELECT, UPDATE, DELETE ON ALL TABLES TO administrateur;
-
+--------------------------------------------
 CREATE ROLE secretaire;
 GRANT SELECT ON ALL TABLES TO secretaire;
 GRANT INSERT,SELECT, DELETE, UPDATE ON rendez_vous TO secretaire;
