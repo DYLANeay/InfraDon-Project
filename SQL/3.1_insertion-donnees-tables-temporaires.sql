dddc04b7f02b45f96a28e578c97a28595a783a64
DYLANeay <dylan.eray6@gmail.com>
2025-05-01 10:01:22 +0200

    changing files location to my wsl folder

--- SQL/3.1_insertion-donnees-tables-temporaires.sql
+++ SQL/3.1_insertion-donnees-tables-temporaires.sql
@@ -1,30 +1,32 @@
+-- Active: 1741351947031@@127.0.0.1@5432@hopital
 -- Remplissez les tables temporaires à l'aide de COPY
 COPY temp_donnes_medecins (id, nom, prenom, specialite, hopital, telephone)
-FROM 'C:\Users\dylan\Desktop\shitty_csv\donnee_medecin.csv' -- changer le chemin d'accès ofc --
+FROM '/tmp/donnee_medecin.csv'
 DELIMITER ';'
 CSV HEADER;
 
+
 COPY temp_assurance_sexes (id, assurance, sexe, adresse_corrigee)
-FROM 'C:\Users\dylan\Desktop\shitty_csv\hopital_dataset_assurances_sexe_adresse.csv' -- changer le chemin d'accès ofc --
+FROM '/tmp/hopital_dataset_assurances_sexe_adresse.csv' -- changer le chemin d'accès ofc --
 DELIMITER ';'
 CSV HEADER;
 
 COPY temp_medicament (id, nom, dosage, type)
-FROM 'C:\Users\dylan\Desktop\shitty_csv\medicament.csv' -- changer le chemin d'accès ofc --
+FROM '/tmp/medicament.csv' -- changer le chemin d'accès ofc --
 DELIMITER ';'
 CSV HEADER;
 
 COPY temp_prescription (id, id_rendez_vous, id_medicament, duree_jours)
-FROM 'C:\Users\dylan\Desktop\shitty_csv\prescription.csv' -- changer le chemin d'accès ofc --
+FROM '/tmp/prescription.csv' -- changer le chemin d'accès ofc --
 DELIMITER ';'
 CSV HEADER;
 
 COPY temp_rendez_vous (id, id_patient, id_medecin, date, motif)
-FROM 'C:\Users\dylan\Desktop\shitty_csv\Rendez_vous.csv' -- changer le chemin d'accès ofc --
+FROM '/tmp/Rendez_vous.csv' -- changer le chemin d'accès ofc --
 DELIMITER ';'
 CSV HEADER;
 
 COPY temp_patient_nonperso (id, nom, prenom, date_de_naissance, adresse, telephone)
-FROM 'C:\Users\dylan\Desktop\shitty_csv\Patient_donnee_nonperso.csv'  -- changer le chemin d'accès ofc --
+FROM '/tmp/Patient_donnee_nonperso.csv'  -- changer le chemin d'accès ofc --
 DELIMITER ';'
 CSV HEADER;
